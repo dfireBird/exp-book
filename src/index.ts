6384e2aa2bafea518f6e8327f768cb18cbcd0727
dfireBird <aadhishrm11@gmail.com>
2020-09-12 21:16:53 +0530

    implement registering with express of author routes

--- src/index.ts
+++ src/index.ts
@@ -3,14 +3,35 @@ import "reflect-metadata";
 import { createConnection } from "typeorm";
 import express, { json } from "express";
 
+import { AuthorRoutes } from "./routes/AuthorRoutes";
+import { Author } from "./entity/Author";
+
 createConnection()
     .then(async (connection) => {
         const app = express();
         app.use(json());
 
-        app.get("/", (req, res) => {
-            res.send(req.body);
-        });
+        const registerRoutes = (route) => {
+            (app as any)[route.method](route.route, (req, res, next) => {
+                const result = new (route.controller as any)()[route.action](
+                    req,
+                    res,
+                    next
+                );
+
+                if (result instanceof Promise) {
+                    result.then((result) =>
+                        result !== null && result !== undefined
+                            ? res.send(result)
+                            : undefined
+                    );
+                } else if (result !== null && result !== undefined) {
+                    res.json(result);
+                }
+            });
+        };
+
+        AuthorRoutes.forEach(registerRoutes);
 
         app.listen(process.env.PORT || 8080);
 
